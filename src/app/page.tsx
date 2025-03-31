a452fc085909005862e17c6c4c01a83d6895db01
amleshwar-p <vinesnimish@gmail.com>
2025-03-31 15:48:16 +0530

    Clerk Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 
 export default function Home() {
+  console.log("Home Page....");
   return (
     <>
-    <div>
-      <h1>Test</h1>
-    </div>
+      <div>
+        <h1>Home page </h1>
+      </div>
     </>
   );
 }
